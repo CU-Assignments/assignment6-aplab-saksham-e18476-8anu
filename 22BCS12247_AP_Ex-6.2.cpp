f81aa7a1c4c18cbc32f1fba26f82e6caa3032fbf
Hemant Vyas <110166331+8anu@users.noreply.github.com>
2025-02-19 16:11:57 +0530

    Add files via upload

---
+++ 22BCS12247_AP_Ex-6.2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if (!root) return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    cout << maxDepth(root) << endl;
+
+    return 0;
+}
